26ca221e00d5df7cb94f827fba16006ae0de8357
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-08-27 12:31:53 +0200

    add comments, draw the punctation gap before the punctation

--- Hidden country/Program.cs
+++ Hidden country/Program.cs
@@ -33,19 +33,39 @@ namespace HiddenCountry
             // check if the title is longer that the longest line with punctations (example: 10. line)
             if (title.Length > longestLineLenght + punctationLength)
             {
-            
-                // draw the box
+                // draw the top line with the title in the middle
                 Console.WriteLine($"+-{title}-+");
-                for(int i = 0; i < lines.Length; i++)
+
+                // draw the inner box
+                for (int i = 0; i < lines.Length; i++)
                 {
+                    // calculate gap for the text line
                     int gap = title.Length - longestLineLenght - punctationLength;
-                    Console.Write($"| {i+1}. {lines[i]}");
+
+                    // draw a left side of the box
+                    Console.Write($"| ");
+
+                    // calculate and draw the punctation gap
+                    int punctationGap = punctationLength - 2 - Convert.ToInt32(Math.Log10(Convert.ToDouble(i + 1)));
+                    for (int j = 0; j < punctationGap; j++)
+                    {
+                        Console.Write(" ");
+                    }
+
+                    // write the punctation with the text line
+                    Console.Write($"{i+1}. {lines[i]}");
+
+                    // draw the text gap
                     for (int j = 0; j < gap; j++)
                     {
                         Console.Write(" ");
                     }
+
+                    //draw right side of the box
                     Console.WriteLine(" |");
                 }
+
+                // draw the bottom side of the box
                 Console.Write("+-");
                 for (int i = 0; i < title.Length; i++)
                 {
