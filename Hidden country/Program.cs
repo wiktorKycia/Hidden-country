7839d537329dad960e82556790d6f35e941b9703
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-08-27 12:05:02 +0200

    settle longest punctation lenght, check if title is the longest line

--- Hidden country/Program.cs
+++ Hidden country/Program.cs
@@ -17,7 +17,9 @@ namespace HiddenCountry
         }
         static void MakeTitleCage(string title, params string[] lines)
         {
-
+            // settle the punctation lenght
+            double len = lines.Length;
+            int punctationLength = Convert.ToInt32(Math.Log10(len)) + 3;
 
             // find the longest line's length
             int longestLineLenght = 0;
@@ -28,7 +30,11 @@ namespace HiddenCountry
                     longestLineLenght = line.Length;
                 }
             }
-            // check if the title is longer that the longest line with punctations (10. line)
+            // check if the title is longer that the longest line with punctations (example: 10. line)
+            if (title.Length > longestLineLenght + punctationLength)
+            {
+
+            }
         }
     }
 }
