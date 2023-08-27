c7ff3fba21c5acef1893e454ea7cde10183f323a
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-08-27 11:29:20 +0200

    Add project files.

---
+++ Hidden country/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HiddenCountry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintColorMessage(ConsoleColor.Magenta, "Witamy w grze Ukryte Państwo!");
+        }
+        static void PrintColorMessage(ConsoleColor color, string message)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
