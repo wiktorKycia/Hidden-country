c66e8594b433759a1f582efa2044f321f1be7d2f
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-08-27 11:42:14 +0200

    add method MakeTitleCage, that will make a cage for information with a title at the top

--- Hidden country/Program.cs
+++ Hidden country/Program.cs
@@ -7,12 +7,24 @@ namespace HiddenCountry
         static void Main(string[] args)
         {
             PrintColorMessage(ConsoleColor.Magenta, "Witamy w grze Ukryte Państwo!");
+
         }
         static void PrintColorMessage(ConsoleColor color, string message)
         {
             Console.ForegroundColor = color;
             Console.WriteLine(message);
             Console.ResetColor();
+        }
+        static void MakeTitleCage(string title, params string[] lines)
+        {
+            int longestLineLenght = 0;
+            foreach (string line in lines)
+            {
+                if (line.Length > title.Length && line.Length > longestLineLenght)
+                {
+                    longestLineLenght = line.Length;
+                }
+            }
         }
     }
 }
